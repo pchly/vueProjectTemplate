6bb642dbd9d44a135b6151e583686855ba8b8d44
Peng chunhao <1650211040@qq.com>
2021-01-29 10:33:00 +0800

    third commit annotate some code

--- src/store/index.js
+++ src/store/index.js
@@ -8,48 +8,50 @@ Vue.use(Vuex);
 const store=new Vuex.Store({
         //状态数据
         state:{
-            topics:[
-                {name:'all',desc:'全部'},
-                {name:'good',desc:'精华'},
-                {name:'share',desc:'分享'},
-                {name:'ask',desc:'问答'},
-                {name:'job',desc:'招聘'},
-                {name:'dev',desc:'测试'}
-              ],
-              unLoginedMoreMenus:{
-                login:'登录'
-              },
-              loginedMoreMenus:{
-                center:'个人中心',
-                createTopic:'发表话题',
-                msg:'消息通知',
-                logout:'退出登录'
-              },
-              isLogined:false,
-              loginName:'',
-              accesstoken:'',
-              isShowMoreMenus:false
+          // 示例数据
+            // topics:[
+            //     {name:'all',desc:'全部'},
+            //     {name:'good',desc:'精华'},
+            //     {name:'share',desc:'分享'},
+            //     {name:'ask',desc:'问答'},
+            //     {name:'job',desc:'招聘'},
+            //     {name:'dev',desc:'测试'}
+            //   ],
+            //   unLoginedMoreMenus:{
+            //     login:'登录'
+            //   },
+            //   loginedMoreMenus:{
+            //     center:'个人中心',
+            //     createTopic:'发表话题',
+            //     msg:'消息通知',
+            //     logout:'退出登录'
+            //   },
+            //   isLogined:false,
+            //   loginName:'',
+            //   accesstoken:'',
+            //   isShowMoreMenus:false
         },
         //状态数据的计算信息
         getters:{
-            moreMenus:function(state){
-                if(state.isLogined==true){
-                  return state.loginedMoreMenus;
-                }
+            // 用法示例
+            // moreMenus:function(state){
+            //     if(state.isLogined==true){
+            //       return state.loginedMoreMenus;
+            //     }
                 
-                return state.unLoginedMoreMenus;
+            //     return state.unLoginedMoreMenus;
                 
-              },
-              getTopicDescByType:function(state){
-                return function(tab){
-                  for(var index=0;index<state.topics.length;index++){
-                    if(state.topics[index].name==tab){
-                      return state.topics[index].desc;
-                    }
-                  }
+            //   },
+            //   getTopicDescByType:function(state){
+            //     return function(tab){
+            //       for(var index=0;index<state.topics.length;index++){
+            //         if(state.topics[index].name==tab){
+            //           return state.topics[index].desc;
+            //         }
+            //       }
 
-                };
-              }
+            //     };
+            //   }
 
         },
         mutations,
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,29 +1,30 @@
 const mutations={
-    mutationIsShowMoreMenus:function(state,payload){
-        //state表示当前的状态数据
-        //payload表示使用方传递的附加数据
-        state.isShowMoreMenus=payload;
-    },
-    mutationLogin:function(state){
-        //state表示当前的状态数据
-        //payload表示使用方传递的附加数据
-        state.isLogined=true;
-    },
-    mutationLoginName:function(state,payload){
-        //state表示当前的状态数据
-        //payload表示使用方传递的附加数据
-        state.loginName=payload;
-    },
-    mutationAccessToken:function(state,payload){
-        //state表示当前的状态数据
-        //payload表示使用方传递的附加数据
-        state.accesstoken=payload;
-    },
-    mutationLogout:function(state){
-        state.isLogined=false;
-        state.loginName='';
-        state.accesstoken='';
-    }
+    // 用法示例
+    // mutationIsShowMoreMenus:function(state,payload){
+    //     //state表示当前的状态数据
+    //     //payload表示使用方传递的附加数据
+    //     state.isShowMoreMenus=payload;
+    // },
+    // mutationLogin:function(state){
+    //     //state表示当前的状态数据
+    //     //payload表示使用方传递的附加数据
+    //     state.isLogined=true;
+    // },
+    // mutationLoginName:function(state,payload){
+    //     //state表示当前的状态数据
+    //     //payload表示使用方传递的附加数据
+    //     state.loginName=payload;
+    // },
+    // mutationAccessToken:function(state,payload){
+    //     //state表示当前的状态数据
+    //     //payload表示使用方传递的附加数据
+    //     state.accesstoken=payload;
+    // },
+    // mutationLogout:function(state){
+    //     state.isLogined=false;
+    //     state.loginName='';
+    //     state.accesstoken='';
+    // }
 
 
 
